f87221e9449b766ea5af436d123b269ba8bf2d2a
PremRajGupta <guptapremraj3@gmail.com>
2025-10-04 22:06:39 +0530

    first commit

---
+++ C LANGUAGE/simple_calculator.c
@@ -0,0 +1,51 @@
+// Write a C program that simulates a simple calculator capable of performing basic arithmetic operations
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+void main()
+{
+    while (1)
+    {
+        printf("Press 1. For Addition Two Numbers.\n");
+        printf("Press 2. For Subtract Two Numbers.\n");
+        printf("Press 3. For Multiple Two Numbers.\n");
+        printf("Press 4. For Division Two Numbers.\n");
+        printf("Press 5. For Exit Two Numbers.\n");
+
+        int option;
+        printf("\nEnter Your Option=");
+        scanf("%d", &option);
+        if (option == 5)
+        {
+            printf("Exit Calculator");
+            exit(0);
+        }
+
+        int num1, num2;
+        printf("\nEnter First Number=");
+        scanf("%d", &num1);
+        printf("Enter Second Number=");
+        scanf("%d", &num2);
+
+        switch (option)
+        {
+        case 1:
+            printf("Sum of Two Number is %d + %d=%d\n", num1, num2, num1 + num2);
+            break;
+        case 2:
+            printf("Subtract of Two Number is %d - %d=%d\n", num1, num2, num1 - num2);
+            break;
+        case 3:
+            printf("Multiple of Two Number is %d * %d=%d\n", num1, num2, num1 * num2);
+            break;
+        case 4:
+            printf("Divide of Two Number is %d / %d=%d\n", num1, num2, num1 / num2);
+            break;
+        default:
+            printf("Your option is wrong, Please try again\n");
+            break;
+        };
+    }
+}
